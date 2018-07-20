e431da3c6447183e66ea4f36a50275731abdd735
CZBDL3@ALLYIPOC.COM <CZBDL3@ALLYIPOC.COM>
2018-07-20 14:30:26 -0400

    added variables to store form info

--- ScrummyBearsProject/src/app/add-task/add-task.component.ts
+++ ScrummyBearsProject/src/app/add-task/add-task.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-add-task',
-  templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  selector: "app-add-task",
+  templateUrl: "./add-task.component.html",
+  styleUrls: ["./add-task.component.css"]
 })
 export class AddTaskComponent implements OnInit {
+  taskAdded = "";
+  taskName: String
+  startDate: String
+  endDate: String
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onClickMe() {
+    this.taskAdded = "Roadmap Updated!"
   }
-
 }
