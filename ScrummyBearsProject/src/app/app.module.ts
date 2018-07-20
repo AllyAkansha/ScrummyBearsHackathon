6995e12f5d3a1fe725019dad8f2b7b8dc47670ea
CZBDL3@ALLYIPOC.COM <CZBDL3@ALLYIPOC.COM>
2018-07-20 12:01:54 -0400

    part of add-task form works

--- ScrummyBearsProject/src/app/app.module.ts
+++ ScrummyBearsProject/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';     
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatFormFieldModule } from '@angular/material';
+
+
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatFormFieldModule,  
+  MatInputModule,      
+  MatDatepickerModule,      
+  MatDatepicker,      
+  MatNativeDateModule,      
+  MatRadioModule,      
+  MatSelectModule,      
+  MatOptionModule,      
+  MatSlideToggleModule,} from '@angular/material';
+  
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { RouterModule, Routes } from '@angular/router';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material';
 
 import { RoadmapViewComponent } from './roadmap-view/roadmap-view.component';
 import { AddTaskComponent } from './add-task/add-task.component';
@@ -43,10 +53,33 @@ const appRoutes: Routes = [
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
-    MatDatepickerModule,
+    MatDatepickerModule,      
     MatFormFieldModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+
+    FormsModule,         
+    MatInputModule,           
+    MatRadioModule,      
+    MatSelectModule,      
+    MatOptionModule,      
+    MatSlideToggleModule 
   ],
+  exports: [      
+    MatButtonModule,      
+    MatMenuModule,      
+    MatToolbarModule,      
+    MatIconModule,      
+    MatCardModule,      
+    BrowserAnimationsModule,      
+    MatFormFieldModule,      
+    MatInputModule,      
+    MatDatepickerModule,      
+    MatNativeDateModule,      
+    MatRadioModule,      
+    MatSelectModule,      
+    MatOptionModule,      
+    MatSlideToggleModule      
+  ],  
   providers: [],
   bootstrap: [AppComponent]
 })
