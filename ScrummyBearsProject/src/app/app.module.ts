a72820b53c15ec9d5b8df68dc8175c94aa862c78
Dinsmoor <PZ263P@INNOVATE.ALLY.COM>
2018-07-20 10:46:12 -0400

    Adding in the navigation for routing

--- ScrummyBearsProject/src/app/app.module.ts
+++ ScrummyBearsProject/src/app/app.module.ts
@@ -8,6 +8,14 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'first-page', component: FirstPageComponent},
+  { path: 'second-page', component: SecondPageComponent},
+  { path: 'third-page', component: ThirdPageComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,6 +25,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,
@@ -25,9 +34,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatMenuModule
+    MatMenuModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
